27adb3e2eaedb6ec1452ae49fdfc54ae18e3a3d4
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-02-08 17:20:08 -0800

    added form functionality. records values, unshifts to entries in with entry ID in data model, resets form.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
 /* global data */
 /* exported data */
 var imgInput = document.querySelector('#new-post-img');
+var formRoute = document.querySelector('form');
+var imgPlaceHolder = document.querySelector('img');
 
 function imgPreview(event) {
   var imgPlaceHolder = document.querySelector('img');
   var newImage = event.target.value;
 
   imgPlaceHolder.setAttribute('src', newImage);
-  if (imgPlaceHolder.getAttribute('src') === '') { imgPlaceHolder.setAttribute('src', 'images/placeholder-image-square.jpg'); }
+  if (imgPlaceHolder.getAttribute('src') === '') {
+    imgPlaceHolder.setAttribute('src', 'images/placeholder-image-square.jpg');
+  }
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  var postInfo = {
+    title: formRoute.title.value,
+    imageURL: formRoute.img.value,
+    notes: formRoute.notes.value,
+    entryID: data.nextEntryId
+  };
+
+  data.nextEntryId++;
+  data.entries.unshift(postInfo);
+  imgPlaceHolder.setAttribute('src', 'images/placeholder-image-square.jpg');
+  formRoute.reset();
 }
 
 imgInput.addEventListener('input', imgPreview);
+formRoute.addEventListener('submit', submitForm);
