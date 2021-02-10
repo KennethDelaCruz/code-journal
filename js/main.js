d1d4f2d427d16cadd2ec30776358110c65a231ca
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-02-10 12:59:08 -0800

    created a function that would generate a post. also created a loop function that would use generatePost and cycle through data.entries to automatically upload all post created from newest to oldest.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 /* global data */
 /* exported data */
-var imgInput = document.querySelector('#new-post-img');
+var imgInput = document.querySelector('#input-image');
 var formRoute = document.querySelector('form');
-var imgPlaceHolder = document.querySelector('img');
+var imgPlaceHolder = document.querySelector('#image-placeholder');
 
 function imgPreview(event) {
-  var imgPlaceHolder = document.querySelector('img');
   var newImage = event.target.value;
 
   imgPlaceHolder.setAttribute('src', newImage);
@@ -23,10 +22,53 @@ function submitForm(event) {
     entryID: data.nextEntryId
   };
   data.nextEntryId++;
-  data.entries.unshift(postInfo);
+  data.entries.push(postInfo);
   imgPlaceHolder.setAttribute('src', 'images/placeholder-image-square.jpg');
   formRoute.reset();
 }
 
 imgInput.addEventListener('input', imgPreview);
 formRoute.addEventListener('submit', submitForm);
+
+function generatePost(object) {
+  var newListItem = document.createElement('li');
+
+  var divRow = document.createElement('div');
+  divRow.setAttribute('class', 'row');
+
+  var divColumnHalf1 = document.createElement('div');
+  divColumnHalf1.setAttribute('class', 'column-half');
+
+  var divColumnHalf2 = document.createElement('div');
+  divColumnHalf2.setAttribute('class', 'column-half');
+
+  var imgElement = document.createElement('img');
+  imgElement.setAttribute('src', object.imageURL);
+
+  var h2Element = document.createElement('h2');
+  h2Element.setAttribute('class', 'post-title');
+  h2Element.textContent = object.title;
+
+  var pElement = document.createElement('p');
+  pElement.textContent = object.notes;
+
+  divColumnHalf2.appendChild(h2Element);
+  divColumnHalf2.appendChild(pElement);
+  divColumnHalf1.appendChild(imgElement);
+  divRow.appendChild(divColumnHalf1);
+  divRow.appendChild(divColumnHalf2);
+  newListItem.appendChild(divRow);
+  return newListItem;
+}
+
+function postLoop(event) {
+  var listRoute = document.querySelector('ul');
+  var objectEntries = data.entries;
+  for (var i = 0; i < objectEntries.length; i++) {
+    var newPost = generatePost(objectEntries[i]);
+    listRoute.prepend(newPost);
+  }
+
+}
+
+document.addEventListener('DOMContentLoaded', postLoop);
